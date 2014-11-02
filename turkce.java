3251c9a3d0c05fe9f5c6b87f58f774af78dc5d9e
gizem <ungovernable_06@hotmail.com>
2014-11-02 13:01:58 +0200

    Create turkce.java
    Random name projesi için gereli olan turkce.java isimli sınıf eklendi.

---
+++ turkce.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class türkce {
+	public static String main() {
+		Random rnd = new Random();	
+		Random rndm = new Random();
+		
+		List<String> isimcik = new ArrayList<String>();
+		isimcik.add ("dünya");
+		isimcik.add ("karpuz");
+		isimcik.add ("kalorifer");
+		isimcik.add ("termostat");
+		isimcik.add ("ayakkabi");
+		isimcik.add ("süpürge");
+		isimcik.add ("havlu");
+		isimcik.add ("oje");
+		isimcik.add ("komidin");
+		isimcik.add ("bardak");
+		
+		List<String> sifatcik = new ArrayList<String>();
+		sifatcik.add("iyi");
+		sifatcik.add("kötü");
+		sifatcik.add("sımarık");
+		sifatcik.add("tombul");
+		sifatcik.add("sicak");
+		sifatcik.add("kokusmus");
+		sifatcik.add("islak");
+		sifatcik.add("bikmis");
+		sifatcik.add("mutlu");
+		sifatcik.add("yeni");
+		
+		
+		int sayi = rnd.nextInt(isimcik.size());
+		int sayi1 = rndm.nextInt(sifatcik.size());
+		String birinci = isimcik.get(sayi);
+		String ikinci = sifatcik.get(sayi1);
+		return ikinci + " " + birinci ;
+		
+	}
+
+}
