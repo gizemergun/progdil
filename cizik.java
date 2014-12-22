bf7d376becf47f79562c54b6f1ca61a21f01ce1c
gizem <ungovernable_06@hotmail.com>
2014-12-23 00:44:18 +0200

    kod görünümü düzeltildi

--- cizik.java
+++ cizik.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 public class cizik {
 	public static void main(String[] args ) {
-		Scanner geldi = new Scanner(System.in) ;
-		String cumle ;
-		System.out.println("lutfen cumle giriniz") ;
-		cumle = geldi.next() ;
+		Scanner geldi = new Scanner(System.in);
+		String cumle;
+		System.out.println("lutfen cumle giriniz");
+		cumle = geldi.next();
 		
-		int i = 1 ;
-		String tutac ;
-		String on ;
-		String arka ;
-		for( i=1; i < ( geldi.length()-1) ; i++ ) {
-			tutac = geldi.substring(i, i+1) ;
-			if( tutac.equals("_") ) {
-				on = geldi.substring(i-1, i) ;
-				arka = geldi.substring(i+1, i+2) ;
-				if( ! on.equals("_") ) {
-					if( ! arka.equals("_") ) {
-						cumle = cumle.substring(0, i) + " " + cumle.substring(i+1, cumle.length()) ;
+		int i = 1;
+		String tutac;
+		String on;
+		String arka;
+		for (i = 1; i < ( geldi.length()-1) ; i++ ){
+			tutac = geldi.substring(i, i+1);
+			if (tutac.equals("_")){
+				on = geldi.substring(i-1, i);
+				arka = geldi.substring(i+1, i+2);
+				if (! on.equals("_")){
+					if (! arka.equals("_")){
+						cumle = cumle.substring(0, i) + " " + cumle.substring(i+1, cumle.length());
 						
 					}
 				}
 				
 			}
-			System.out.println(cumle) ;
+			System.out.println(cumle);
 			
 		}
 		
